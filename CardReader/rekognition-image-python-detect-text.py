161f5b401149af5de3f25725d4ad2217a3fa7200
Rafiq Gasmi <Rafiq.Gasmi@collinsongroup.com>
2019-11-15 11:50:50 +0000

    Clean up

--- CardReader/rekognition-image-python-detect-text.py
+++ CardReader/rekognition-image-python-detect-text.py
@@ -10,45 +10,64 @@ card = ""
 font = cv2.FONT_HERSHEY_COMPLEX
 img_name = "card.png"
 card = ""
+bucket = 'collinsoncards'
+photo = 'card.png'
+oldCard = "card"
 
 
-def process():
-    bucket = 'collinsoncards'
-    photo = 'card.png'
+def dmc_detect():
+    Dmc = decode(Image.open(photo))
 
+    if (len(Dmc) != 0) and (len(Dmc[0].data) > 0):
+        print Dmc[0].data.split('/')[4]
+        return Dmc[0].data.split('/')[4]
+
+    return ""
+
+
+def upload_image_to_s3():
     s3 = boto3.resource('s3')
 
     data = open(img_name, 'rb')
     s3.Bucket('collinsoncards').put_object(Key='card.png', Body=data)
 
+
+def process_image():
     client = boto3.client('rekognition')
 
-    Dmc = decode(Image.open(photo))
-
-    if (len(Dmc) != 0) and (len(Dmc[0].data) > 0):
-        card = Dmc[0].data.split('/')[4]
+    return client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
 
 
-    response = client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
+def process():
 
-    textDetections = response['TextDetections']
-    oldCard = "card"
-    for text in textDetections:
+    global oldCard
+    dmcCard = dmc_detect()
+    if dmcCard != "":
+        if dmcCard == oldCard:
+            print 'old'
+        oldCard = dmcCard
+        return dmcCard
+
+    upload_image_to_s3()
+    response = process_image()
+
+    for text in response['TextDetections']:
 
         if (text['DetectedText'].isdigit()) and len(text['DetectedText']) == 10:
             if oldCard == text['DetectedText']:
-                print("old card")
+                print 'old card'
+                break
             else:
-                card = text['DetectedText']
-                print card
-                oldCard = card
-
+                oldCard = text['DetectedText']
+                print text['DetectedText']
+                return text['DetectedText']
+            print 'fail'
 
 while True:
     ret, frame = cam.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0, 132, 0])
-    upper_red = np.array([129, 255, 138])
+    lower_red = np.array([0, 48, 13])
+    upper_red = np.array([180, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.erode(mask, kernel)
@@ -61,9 +80,8 @@ while True:
         y = approx.ravel()[1]
 
         if area > 400:
-            cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
+            #cv2.drawContours(frame, [approx], 0, (0, 0, 0), 5)
             if len(approx) == 4:
-                cv2.putText(frame, "Rectangle", (x, y), font, 1, (0, 0, 0))
                 cv2.imwrite(img_name, frame)
                 process()
 
